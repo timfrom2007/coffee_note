0c8d95746e3c22b459595f9af5e5ae6594765f3f
張庭韶 <timfrom2007@gmail.com>
2015-07-22 14:09:47 +0800

    加了coffeeN.js

---
+++ js/coffeeN.js
@@ -0,0 +1,8 @@
+
+$(document).ready(function(){ //function 內的內容會在整個原始黨載入完成後執行
+    //$().aaa();
+    $('#cost-limit li').hover(function(){
+        $('#cost-limit li').css("color","red");
+    });
+    
+});
