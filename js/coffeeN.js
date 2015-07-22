6e21976ae7fac210f3541e443f53d15689a2f692
cathyshihs <owlwinner@gmail.com>
2015-07-22 16:16:45 +0800

    cut header and add color change in selectors

--- js/coffeeN.js
+++ js/coffeeN.js
@@ -1,8 +1,41 @@
 $(document).ready(function(){ //function 內的內容會在整個原始黨載入完成後執行
-    
-    $('.test li').click(function(){
-        $('.test li').css("color","black");
-        $(this).css("color","red");
-    });
-    
+
+// $('#cost-limit li').on("mouseenter",(function(){
+//     $(this).css("color","red");//#8B4513
+// });
+// $('#cost-limit li').mouseleave(function(){
+//     $(this).css("color","#8B4513");
+// });
+
+ // $('#cost-limit li').mouseenter(function(){
+ //        $(this).css("color","blue");
+ //    });
+$('#cost-limit li').click(function(){
+    $('#cost-limit li').css("color","black");
+    $(this).css("color","red");
+});
+// $('#cost-limit li').on("mouseenter",function(){
+// 	if($('#cost-limit li').css("color","red"))){
+// 	}else{
+// 		$('#cost-limit li').css("color","black");
+//    		$(this).css("color","blue");
+// 	}
+// });
+$('#time-limit li').click(function(){
+    $('#time-limit li').css("color","black");
+    $(this).css("color","red");
+});
+$('#wifi-limit li').click(function(){
+    $('#wifi-limit li').css("color","black");
+    $(this).css("color","red");
+});
+$('#plug-limit li').click(function(){
+    $('#plug-limit li').css("color","black");
+    $(this).css("color","red");
+});
+$('#pet-limit li').click(function(){
+    $('#pet-limit li').css("color","black");
+    $(this).css("color","red");
+});
+
 });
