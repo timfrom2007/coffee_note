19e49ebf0e644b12779a5de9d10ffb764c93f5a5
cathyshihs <owlwinner@gmail.com>
2015-07-25 00:26:59 +0800

    no message

--- js/coffeeN.js
+++ js/coffeeN.js
@@ -4,33 +4,59 @@ $('#cost-limit li').on("mouseenter",function(){
     $(this).css("color","#FF8C00");
 });
 $('#cost-limit li').on("mouseleave",function(){
-    $(this).css("color","#8B4513");
+    $(this).css("color","black");
 });
-
 $('#cost-limit li').click(function(){
     $('#cost-limit li').css("color","black");
     $(this).css("color","red");
 });
+
 // $('#cost-limit li').on("mouseenter",function(){
-// 	if($('#cost-limit li').css("color","red"))){
+// 	var isRed=$(this).css('color');
+
+//     if(isRed.equals"red")){
 // 	}else{
-// 		$('#cost-limit li').css("color","black");
+// 		// $('#cost-limit li').css("color","black");
 //    		$(this).css("color","blue");
 // 	}
 // });
+$('#time-limit li').on("mouseenter",function(){
+    $(this).css("color","#FF8C00");
+});
+$('#time-limit li').on("mouseleave",function(){
+    $(this).css("color","black");
+});
 $('#time-limit li').click(function(){
     $('#time-limit li').css("color","black");
     $(this).css("color","red");
+});
+$('#wifi-limit li').on("mouseenter",function(){
+    $(this).css("color","#FF8C00");
+});
+$('#wifi-limit li').on("mouseleave",function(){
+    $(this).css("color","black");
 });
 $('#wifi-limit li').click(function(){
     $('#wifi-limit li').css("color","black");
     $(this).css("color","red");
 });
-$('#plug-limit li').click(function(){
+$('#eat-limit li').on("mouseenter",function(){
+    $(this).css("color","#FF8C00");
+});
+$('#eat-limit li').on("mouseleave",function(){
+    $(this).css("color","black");
+});
+$('#eat-limit li').click(function(){
     $('#plug-limit li').css("color","black");
     $(this).css("color","red");
 });
-$('#pet-limit li').click(function(){
+$('#other-limit li').on("mouseenter",function(){
+    $(this).css("color","#FF8C00");
+});
+$('#other-limit li').on("mouseleave",function(){
+    $(this).css("color","black");
+});
+$('#other-limit li').click(function(){
     $('#pet-limit li').css("color","black");
     $(this).css("color","red");
 });
