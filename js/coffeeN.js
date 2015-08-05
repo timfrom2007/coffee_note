86e1072cbbcc71d090ba674b96618b8479eb954b
張庭韶 <timfrom2007@gmail.com>
2015-08-05 14:22:38 +0800

    把篩選器衝突者做單一選擇

--- js/coffeeN.js
+++ js/coffeeN.js
@@ -1,68 +1,97 @@
 
 $(document).ready(function(){ //function 內的內容會在整個原始黨載入完成後執行
     $( "#cost-item-0" ).change(function() {
-      if($(this).prop('checked')){
+      if($(this).prop('checked')){  //先將所有選取改成false,另外再把此改成true
         // var hihi=$(this) ;
         // console.log(hihi);
+        $("#cost-limit input").prop('checked', false);  
+        $("#cost-limit li").removeClass('is-acted');
+        $(this).prop('checked', true);
         $(this).parent().parent().find('.cost-0').addClass('is-acted');
       }else{
+        
         $(this).parent().parent().find('.cost-0').removeClass('is-acted');
       }
     }).change();
-    $( "#cost-item-100" ).change(function() {
+    
+    $( "#cost-item-100" ).change(function() {  //先將所有選取改成false,另外再把此改成true
       if($(this).prop('checked')){
         // var hihi=$(this) ;
         // console.log(hihi);
+        $("#cost-limit input").prop('checked', false);
+        $("#cost-limit li").removeClass('is-acted');
+        $(this).prop('checked', true);
         $(this).parent().parent().find('.cost-100').addClass('is-acted');
       }else{
         $(this).parent().parent().find('.cost-100').removeClass('is-acted');
       }
     }).change();
-    $( "#cost-item-drink" ).change(function() {
+    
+    $( "#cost-item-drink" ).change(function() {  //先將所有選取改成false,另外再把此改成true
       if($(this).prop('checked')){
         // var hihi=$(this) ;
         // console.log(hihi);
+        $("#cost-limit input").prop('checked', false);
+        $("#cost-limit li").removeClass('is-acted');
+        $(this).prop('checked', true);
         $(this).parent().parent().find('.cost-drink').addClass('is-acted');
       }else{
         $(this).parent().parent().find('.cost-drink').removeClass('is-acted');
       }
     }).change();
-    $( "#cost-item-eat" ).change(function() {
+    
+    $( "#cost-item-eat" ).change(function() {  //先將所有選取改成false,另外再把此改成true
       if($(this).prop('checked')){
         // var hihi=$(this) ;
         // console.log(hihi);
+        $("#cost-limit input").prop('checked', false);
+        $("#cost-limit li").removeClass('is-acted');
+        $(this).prop('checked', true);
         $(this).parent().parent().find('.cost-eat').addClass('is-acted');
       }else{
         $(this).parent().parent().find('.cost-eat').removeClass('is-acted');
       }
     }).change();
+    
     $( "#time-item-0" ).change(function() {
       if($(this).prop('checked')){
         // var hihi=$(this) ;
         // console.log(hihi);
+        $("#time-limit input").prop('checked', false);
+        $("#time-limit li").removeClass('is-acted');
+        $(this).prop('checked', true);
         $(this).parent().parent().find('.time-0').addClass('is-acted');
       }else{
         $(this).parent().parent().find('.time-0').removeClass('is-acted');
       }
     }).change();
+    
     $( "#time-item-1" ).change(function() {
       if($(this).prop('checked')){
         // var hihi=$(this) ;
         // console.log(hihi);
+        $("#time-limit input").prop('checked', false);
+        $("#time-limit li").removeClass('is-acted');
+        $(this).prop('checked', true);
         $(this).parent().parent().find('.time-1').addClass('is-acted');
       }else{
         $(this).parent().parent().find('.time-1').removeClass('is-acted');
       }
     }).change();
+    
     $( "#time-item-2" ).change(function() {
       if($(this).prop('checked')){
         // var hihi=$(this) ;
         // console.log(hihi);
+        $("#time-limit input").prop('checked', false);
+        $("#time-limit li").removeClass('is-acted');
+        $(this).prop('checked', true);
         $(this).parent().parent().find('.time-2').addClass('is-acted');
       }else{
         $(this).parent().parent().find('.time-2').removeClass('is-acted');
       }
     }).change();
+    
     $( "#wifi-item-wifi" ).change(function() {
       if($(this).prop('checked')){
         // var hihi=$(this) ;
@@ -137,72 +166,13 @@ $(document).ready(function(){ //function 內的內容會在整個原始黨載入
     }).change();
 
 
-// $('#cost-limit li').on("mouseenter",function(){
-//     $(this).css("color","#FF8C00");
-// });
-// $('#cost-limit li').on("mouseleave",function(){
-//     $(this).css("color","black");
-// });
-// $('#cost-limit li').click(function(){
-//     $('#cost-limit li').css("color","black");
-//     $(this).css("color","red");
-// });
 
+    $('#articleChange-color').on("mouseenter",function(){
+        $(this).css("color","#FF8C00");
+    });
 
-// $('#cost-limit li').on("mouseenter",function(){
-// 	var isRed=$(this).css('color');
-
-//     if(isRed.equals"red")){
-// 	}else{
-// 		// $('#cost-limit li').css("color","black");
-//    		$(this).css("color","blue");
-// 	}
-// });
-$('#time-limit li').on("mouseenter",function(){
-    $(this).css("color","#FF8C00");
-});
-$('#time-limit li').on("mouseleave",function(){
-    $(this).css("color","black");
-});
-$('#time-limit li').click(function(){
-    $('#time-limit li').css("color","black");
-    $(this).css("color","red");
-});
-$('#wifi-limit li').on("mouseenter",function(){
-    $(this).css("color","#FF8C00");
-});
-$('#wifi-limit li').on("mouseleave",function(){
-    $(this).css("color","black");
-});
-$('#wifi-limit li').click(function(){
-    $('#wifi-limit li').css("color","black");
-    $(this).css("color","red");
-});
-$('#eat-limit li').on("mouseenter",function(){
-    $(this).css("color","#FF8C00");
-});
-$('#eat-limit li').on("mouseleave",function(){
-    $(this).css("color","black");
-});
-$('#eat-limit li').click(function(){
-    $('#plug-limit li').css("color","black");
-    $(this).css("color","red");
-});
-$('#other-limit li').on("mouseenter",function(){
-    $(this).css("color","#FF8C00");
-});
-$('#other-limit li').on("mouseleave",function(){
-    $(this).css("color","black");
-});
-$('#other-limit li').click(function(){
-    $('#pet-limit li').css("color","black");
-    $(this).css("color","red");
-});
-$('#articleChange-color').on("mouseenter",function(){
-    $(this).css("color","#FF8C00");
-});
-$('#articleChange-color').on("mouseleave",function(){
-    $(this).css("color","#8B4513");
-});
+    $('#articleChange-color').on("mouseleave",function(){
+        $(this).css("color","#8B4513");
+    });
 
 });
